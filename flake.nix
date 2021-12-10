c4cf15c6c2056e7d2969d32076c36ad89310cda1
ishiy <y.ishihara.1993@gmail.com>
2021-12-10 20:18:37 +0900

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -4,24 +4,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }: (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
         py = pkgs.python38;
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.protobuf
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.protobuf
 
-          py
-          py.pkgs.python-language-server
-          py.pkgs.jedi
-          py.pkgs.poetry
-        ];
+            py
+            py.pkgs.jedi-language-server
+            py.pkgs.poetry
+          ];
 
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]}
-        '';
-      };
-    }
-  );
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]}
+          '';
+        };
+      }
+    );
 }
